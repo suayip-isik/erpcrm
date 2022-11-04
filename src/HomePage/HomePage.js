015159b647853df99914c8c5e7aee7d32b2589c6
Şuayip Işık <101035734+suayip-isik@users.noreply.github.com>
2022-11-04 08:19:51 -0700

    usestate added

--- src/HomePage/HomePage.js
+++ src/HomePage/HomePage.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Text, View, SafeAreaView, StyleSheet, TouchableOpacity, TextInput, Button, ScrollView } from "react-native";
 // import { WebView } from "react-native-webview";
 import { WebView } from 'react-native-webview';
